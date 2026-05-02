3f8b1ad9d2f31075ac48b7b7caeaeff9fbff3892
tokamcodes <tokamcodes@gmail.com>
2026-05-02 13:31:02 +0100

    Added in functionality to place the mines - Basic cell background colour added for a clear cell, flag placed and mine clicked. Created simple array for board size and mines required and utilised a variable for when setting up or changing the board size. Also added in a check to reverse a flag being placed. Need to refacrtor how the cell clicks are processed and utilise an array of the cells  ready for checking adjacent cells. Also need to add in visuals for the number of mines nearby.

--- src/js/minesweeper.js
+++ src/js/minesweeper.js
@@ -1,15 +1,32 @@
-const GRID_SIZE = 10;
+const small_board = [9, 10]; //default grid size with 9 rows/cols and 10 mines.
+const medium_board = [15, 40];
+const large_board = [20, 60];
+
+let grid_size = small_board; 
 let gameBoard;
+let gameCells;
+let minesPlaced = false; //On first user click, we want to place the mines. This allows the user to have a safe first click.
 
 document.addEventListener("DOMContentLoaded", () => {    
     gameBoard = document.getElementById("minesweeper_grid");
+    gameCells = document.getElementsByClassName("ms_cell");
 
     gameBoard.addEventListener("contextmenu", (event) => {
+        //Flag placement (right click) restricted to just the cells of the game board to prevent context menu showing.
         let cell = event.target;
         event.preventDefault();
         if(cell.classList.contains("ms_cell")){
             placeFlag(cell);  
         } 
+    });
+
+    gameBoard.addEventListener("click", (event) => {        
+        evaluateCell(event.target);
+            //May need to refactor this if the click event covers more than just the left mouse button.
+            if (!minesPlaced){
+            placeMines();
+        }
+
     });
     
     const sm_button = document.getElementById("ms_small");
@@ -17,28 +34,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const lg_button = document.getElementById("ms_large");
 
     sm_button.addEventListener("click", () => {
-        createBoard(GRID_SIZE);
+        grid_size = small_board;
+        createBoard();
     });
 
     md_button.addEventListener("click", () => {
-        createBoard(15);
+        grid_size = medium_board;
+        createBoard();
     });             
 
     lg_button.addEventListener("click", () => {
-        createBoard(20);
+        grid_size = large_board;
+        createBoard();
     });
 
     
     const restartButton = document.getElementById("restart_game");
     restartButton.addEventListener("click", () => {
         //Need to enhance this for user confirmation - need to avoid accidental clicks.
-        createBoard(GRID_SIZE);
+        //reset basic board size to default.
+        grid_size = small_board;
+        createBoard();
+        minesPlaced = false;
     });
 
-    createBoard(GRID_SIZE);
+    createBoard();
 });
 
-function createBoard(size){
+function createBoard(){
+    let size = grid_size[0] 
     gameBoard.innerHTML = "";
     for(let c = 0; c < size; c++){
         for(let r = 0; r < size; r++){
@@ -54,5 +78,47 @@ function createBoard(size){
 }
 
 function placeFlag(cell){
-    cell.style.backgroundColor = "red";
-}
+    if ('flag' in cell.dataset){
+        delete cell.dataset.flag;
+        return;
+    }
+    cell.dataset.flag = "true";
+};
+
+function placeMines(){
+    let number_of_mines = grid_size[1];
+    let cells = Array.from(gameCells);
+    for (let m = 0; m < number_of_mines; m++){
+        let randomCellIndex = Math.floor(Math.random() * cells.length)
+        if(!cells[randomCellIndex].dataset.mine){
+            cells[randomCellIndex].dataset.mine = "true";
+        }else{
+            m--; //Reduce the mine counter, without this we may not get the required number of mines if a previously random cell has been selected again.
+        }
+    }
+    minesPlaced = true;
+};
+
+function evaluateCell(cell){
+    if(cell.dataset.mine){
+        //This will be game over...need to code for this properly.
+        cell.dataset.mineclicked = "true";
+        alert("Game Over! You hit a mine.");
+        return;
+    }
+    checkAdjacentCells(cell);
+};
+
+function checkAdjacentCells(selectedCell){
+//Need to consider if the cell is on the edge of the board - do not want out of bounds errors.
+
+    clearCell(selectedCell)
+};
+
+function clearCell(cell){
+    cell.dataset.cleared = "true";
+};
+
+function applyMineCount(cell, count){
+
+};
